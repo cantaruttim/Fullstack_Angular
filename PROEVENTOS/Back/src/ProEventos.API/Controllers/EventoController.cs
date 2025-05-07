639d69675a933115170b1282334047493ea72fbc
Matheus Cantarutti <cantaruttim@outlook.com>
2025-05-07 01:19:51 -0300

    get

---
+++ PROEVENTOS/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5.0",
+                Local = "São Paulo",
+                Lote = "1",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "local.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "Novidades",
+                Local = "São Paulo",
+                Lote = "2",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
+                ImagemURL = "local.png"
+            }
+        };
+
+        public EventoController(ILogger<EventoController> logger)
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+    }
+}
+
